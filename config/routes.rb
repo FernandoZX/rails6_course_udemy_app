25b444e5211bd25c31a2469adff503c17f66fafc
Fernando Cabrera <ingfernandoc@gmail.com>
2022-08-03 14:49:29 -0500

    add home controller and remove static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :courses
-  root 'static_pages#landing_page'
-  get 'privacy_policy', to: 'static_pages#privacy_policy'
+  get 'home/index'
+  root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
