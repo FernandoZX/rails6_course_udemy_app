215be7bd663db75335dd57b576611f7f405771e2
Fernando Cabrera <ingfernandoc@gmail.com>
2022-08-05 00:11:10 -0500

    add style views with bootstrap 5 and friendly for courses

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,7 @@
 class Course < ApplicationRecord
+  extend FriendlyId
   belongs_to :user
+  friendly_id :title, use: :slugged
   has_rich_text :description
   validates :title,  presence: true
   validates :description, presence: true, length: { :minimum => 5 }
