78737918fa3cc4e32d49d40d27538615293d0066
Fernando Cabrera <ingfernandoc@gmail.com>
2022-08-09 10:26:43 -0500

    Add language's level short description price to course's model

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,7 +3,7 @@ class Course < ApplicationRecord
   belongs_to :user
   friendly_id :title, use: :slugged
   has_rich_text :description
-  validates :title,  presence: true
+  validates :title, :short_description, :language, :price, :level,  presence: true
   validates :description, presence: true, length: { :minimum => 5 }
   def to_s
     title
